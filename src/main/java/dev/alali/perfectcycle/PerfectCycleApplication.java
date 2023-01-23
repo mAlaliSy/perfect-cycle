bdde19bf8aa1df19a19231d01c6b8b5e877f0797
Mohammad AlAli <m@alali.dev>
2023-01-23 08:04:52 +0300

    Initial commit

---
+++ src/main/java/dev/alali/perfectcycle/PerfectCycleApplication.java
@@ -0,0 +1,13 @@
+package dev.alali.perfectcycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfectCycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerfectCycleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/alali/perfectcycle/PerfectCycleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alali.perfectcycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfectCycleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
